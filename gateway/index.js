a790e7fa9d41663a754b0fb8c6e39743e94e8f6f
Anton Antoniades <anton.antoniades@zoho.com>
2020-09-08 22:58:21 +1000

    Implemented federation with Apollo Gateway

---
+++ gateway/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "movie-studio", url: "http://localhost:4100/gql" },
+    { name: "movie-theatre", url: "http://localhost:4200/gql" },
+  ],
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+  gateway,
+
+  // Disable subscriptions (not currently supported with ApolloGateway)
+  subscriptions: false,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
